2f43c76dcbd1d7c9ac81558939f0f86a1505338e
zachu <zachu90@gmail.com>
2017-06-15 20:44:08 +0100

    Implemented handlebars.js and created templates for list, task and list of tasks

--- resources/js/todo.js
+++ resources/js/todo.js
@@ -4,35 +4,43 @@
  * List is a collection of tasks
  */
 class List {
+  // Create a list
   constructor(title) {
     this.title = title;
     this.tasks = new Array();
   }
 
+  // Create list title
   setTitle(title) {
     this.title = title;
   }
 
+  // Fetch title
   getTitle() {
     return this.title;
   }
 
+  // Fetch tasks array
   getTasks() {
     return this.tasks;
   }
 
+  // Add task to the array
   addTask(task) {
     this.tasks.push(task);
   }
 
+  // Render list of tasks template
   render() {
-    var tasks = '<ul>';
-    $.each(this.tasks, function(index, task) {
-      tasks += '<li>' + Task.render(task) + '</li>';
-    });
+    var tasks = Task.renderAll(this.tasks);
+    var source = $("script#list-template").html();
+    var template = Handlebars.compile(source);
+    var data = {
+      title: this.title,
+      tasks: tasks
+    }
 
-    tasks += '</ul>';
-    return '<h1>' + this.title + '</h1>' + tasks;
+    return template(data);
   }
 }
 
@@ -40,12 +48,45 @@ class List {
  * Structure and behaviour of individual task
  */
 class Task {
+  // Create a task
   constructor(title, content) {
     this.title = title;
     this.content = content;
   }
 
+  // Render tasks list template
+  static renderAll(tasksArray) {
+
+    var tasks = new Array;
+    $.each(tasksArray, function(index, task) {
+      tasks.push(Task.render(task));
+    });
+
+    var source = $("script#tasks-template").html();
+    var template = Handlebars.compile(source);
+    var data = {
+      tasks: tasks
+    }
+
+    return template(data);
+  }
+
+  // Render task template
   static render(task) {
-    return task.title + ': ' + task.content
+    var source = $("script#task-template").html();
+    var template = Handlebars.compile(source);
+    var data = {
+      title: task.title,
+      content: task.content
+    }
+
+    return template(data);
   }
 }
+
+/**
+ * Handle events
+ */
+$(document).ready(function() {
+  // All events will be handled here
+});
